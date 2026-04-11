f2ae31543bc7f1b94acd5e57bde3155b168cbce1
CXPOH <cxpoh1988@gmail.com>
2026-04-11 16:23:51 +0300

    Добавил библиотеки в гредл, создай файлы настроек и АПИ

---
+++ src/main/java/core/clients/APIClients.java
@@ -0,0 +1,31 @@
+package core.clients;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class APIClients {
+
+    private final String baseUrl;
+
+    public APIClients() {
+        this.baseUrl = determineBaseUrl();
+    }
+    private String determineBaseUrl() {
+        String environment = System.getProperty("env","test");
+        String configFileName = "application-" + environment + ".properties";
+
+        Properties properties = new Properties();
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream(configFileName)){
+        if (input == null) {
+            throw new IllegalStateException("Не найден файл конфигурации" + configFileName);
+
+        }
+            properties.load(input);
+        } catch (IOException e) {
+            throw new IllegalStateException("Не найден файл конфигурации" + configFileName, e);
+        }
+
+        return properties.getProperty("baseUrl");
+    }
+}
