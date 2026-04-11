109144eef8d1d5db466e6cf52feadb40720f0a5c
CXPOH <cxpoh1988@gmail.com>
2026-04-11 16:29:05 +0300

    Сделал выравнивание

--- src/main/java/core/clients/APIClients.java
+++ src/main/java/core/clients/APIClients.java
@@ -11,16 +11,17 @@ public class APIClients {
     public APIClients() {
         this.baseUrl = determineBaseUrl();
     }
+
     private String determineBaseUrl() {
-        String environment = System.getProperty("env","test");
+        String environment = System.getProperty("env", "test");
         String configFileName = "application-" + environment + ".properties";
 
         Properties properties = new Properties();
-        try (InputStream input = getClass().getClassLoader().getResourceAsStream(configFileName)){
-        if (input == null) {
-            throw new IllegalStateException("Не найден файл конфигурации" + configFileName);
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream(configFileName)) {
+            if (input == null) {
+                throw new IllegalStateException("Не найден файл конфигурации" + configFileName);
 
-        }
+            }
             properties.load(input);
         } catch (IOException e) {
             throw new IllegalStateException("Не найден файл конфигурации" + configFileName, e);
