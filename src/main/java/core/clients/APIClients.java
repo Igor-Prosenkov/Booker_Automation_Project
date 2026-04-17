8496ee9915414c199f6a2e3945b832d8d916319c
CXPOH <cxpoh1988@gmail.com>
2026-04-17 23:45:42 +0300

    переписал тесты на check из урока

--- src/main/java/core/clients/APIClients.java
+++ src/main/java/core/clients/APIClients.java
@@ -1,8 +1,12 @@
 package core.clients;
-
+import core.settings.ApiEndpoints;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+
 
 public class APIClients {
 
@@ -29,4 +33,36 @@ public class APIClients {
 
         return properties.getProperty("baseUrl");
     }
+
+    //Настройка для отправки параметров
+    private RequestSpecification getRequestSpec() {
+        return RestAssured.given()
+                .baseUri(baseUrl)
+                .header("Content-Type","application/json")
+                .header("Content-Type", "application/json");
+
+    }
+
+        //get для ping
+    public Response ping() {
+        return getRequestSpec()
+                .when()
+                .get(ApiEndpoints.PING.getPath())
+                .then()
+                .statusCode(201)
+                .extract()
+                .response();
+    }
+
+
+    public Response getBooking() {
+        return getRequestSpec()
+                .when()
+                .get(ApiEndpoints.BOOKING.getPath()) // Используем ENUM для эндпоинта /booking
+                .then()
+                .statusCode(200) // Ожидаемый статус-код 200 OK
+                .extract()
+                .response();
+    }
+
 }
